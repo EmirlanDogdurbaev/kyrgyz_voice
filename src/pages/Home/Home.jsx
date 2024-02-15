dc66ef1f3a51fda1ab9eccf74caf2cf8928feccd
EmirlanDogdurbaev <jigit38@gmail.com>
2024-02-15 19:49:43 +0600

    added home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,36 @@
-import classes from "./Home.module.scss"
+import { Link } from "react-router-dom";
+import classes from "./Home.module.scss";
+import { useRef } from "react";
 
 const Home = () => {
+  const targetElementRef = useRef(null);
+  const scrollToTargetElement = () => {
+    if (targetElementRef.current) {
+      targetElementRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className={classes.Home}>
-      <section>
+      <section className={classes.Header}>
         <h1>home</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius at,
           tempora nisi porro itaque ut alias. Illo at impedit possimus placeat
           velit, eos tempore? Hic eum omnis autem dolores.
         </p>
+        <button onClick={scrollToTargetElement}>Начать Обучение</button>
+      </section>
+
+      <section id="targetElement" ref={targetElementRef}>
+        <article>
+          <h3>asdas</h3>
+        </article>
+        <article>
+          <h3>asdas</h3>
+        </article>
+        <article>
+          <h3>asdas</h3>
+        </article>
       </section>
     </div>
   );
